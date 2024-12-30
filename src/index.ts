ba0b5cdf6cdd5e6a07aba53ca8433ab9225115c7
Aldo Arista <aristaaldo2@gmail.com>
2024-12-30 18:53:49 +0800

    configure typescript

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import express from "express";
+
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  req;
+  res.send("Test");
+});
+
+app.listen(port, () => {
+  console.log("server started at http://localhost:" + port);
+});
